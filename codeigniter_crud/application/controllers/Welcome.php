508646e5de48d441b54809197e7c6050b6c3d2fa
parvez <parvezms60@gmail.com>
2021-04-28 01:14:14 +0530

    new changes done

--- codeigniter_crud/application/controllers/Welcome.php
+++ codeigniter_crud/application/controllers/Welcome.php
@@ -21,5 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		$this->load->view('welcome_message1');
+		$this->load->view('welcome_message2');
 	}
 }
